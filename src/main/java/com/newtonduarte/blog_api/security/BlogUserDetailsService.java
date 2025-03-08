5ef260d3f47320ebf43efa6a0fc6ab1e2c061b02
Newton Duarte Cavalcanti <newton_@hotmail.com>
2025-03-08 16:46:30 -0300

    feat: add blog user details service

---
+++ src/main/java/com/newtonduarte/blog_api/security/BlogUserDetailsService.java
@@ -0,0 +1,21 @@
+package com.newtonduarte.blog_api.security;
+
+import com.newtonduarte.blog_api.domain.entities.User;
+import com.newtonduarte.blog_api.repositories.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+public class BlogUserDetailsService implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(email).orElseThrow(() -> new UsernameNotFoundException("User not found with email: " + email));
+
+        return new BlogUserDetails(user);
+    }
+}
