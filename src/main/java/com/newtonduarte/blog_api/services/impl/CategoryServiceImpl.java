0dfbc503b20dcaad4fe8f5847457fe9cd20e872f
Newton Duarte Cavalcanti <newton_@hotmail.com>
2025-03-08 12:17:27 -0300

    feat: implement create category service

--- src/main/java/com/newtonduarte/blog_api/services/impl/CategoryServiceImpl.java
+++ src/main/java/com/newtonduarte/blog_api/services/impl/CategoryServiceImpl.java
@@ -3,6 +3,7 @@ package com.newtonduarte.blog_api.services.impl;
 import com.newtonduarte.blog_api.domain.entities.Category;
 import com.newtonduarte.blog_api.repositories.CategoryRepository;
 import com.newtonduarte.blog_api.services.CategoryService;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -16,5 +17,16 @@ public class CategoryServiceImpl implements CategoryService {
     @Override
     public List<Category> listCategories() {
         return categoryRepository.findAllWithPostCount();
+    }
+
+    @Override
+    @Transactional
+    public Category createCategory(Category category) {
+        String categoryName = category.getName();
+        if (categoryRepository.existsByNameIgnoreCase(categoryName)) {
+            throw new IllegalArgumentException("Category already exists with name: " + categoryName);
+        }
+
+        return categoryRepository.save(category);
     }
 }
