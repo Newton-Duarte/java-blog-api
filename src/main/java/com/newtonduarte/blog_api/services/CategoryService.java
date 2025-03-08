2a839f85714a282d06a00fd8ef928c51f4d9897d
Newton Duarte Cavalcanti <newton_@hotmail.com>
2025-03-08 16:11:21 -0300

    feat: add delete category method to category interface

--- src/main/java/com/newtonduarte/blog_api/services/CategoryService.java
+++ src/main/java/com/newtonduarte/blog_api/services/CategoryService.java
@@ -3,8 +3,10 @@ package com.newtonduarte.blog_api.services;
 import com.newtonduarte.blog_api.domain.entities.Category;
 
 import java.util.List;
+import java.util.UUID;
 
 public interface CategoryService {
     List<Category> listCategories();
     Category createCategory(Category category);
+    void deleteCategory(UUID id);
 }
