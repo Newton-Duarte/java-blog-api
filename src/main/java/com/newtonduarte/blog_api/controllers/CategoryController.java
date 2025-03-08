7cb34241b3fbce69583e45d0d2167a41952c9f6f
Newton Duarte Cavalcanti <newton_@hotmail.com>
2025-03-08 12:02:14 -0300

    feat: add category controller

---
+++ src/main/java/com/newtonduarte/blog_api/controllers/CategoryController.java
@@ -0,0 +1,29 @@
+package com.newtonduarte.blog_api.controllers;
+
+import com.newtonduarte.blog_api.domain.dtos.CategoryDto;
+import com.newtonduarte.blog_api.mappers.CategoryMapper;
+import com.newtonduarte.blog_api.services.CategoryService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(path = "/api/v1/categories")
+@RequiredArgsConstructor
+public class CategoryController {
+    private final CategoryService categoryService;
+    private final CategoryMapper categoryMapper;
+
+    @GetMapping
+    public ResponseEntity<List<CategoryDto>> listCategories() {
+        List<CategoryDto> categories = categoryService
+                .listCategories()
+                .stream().map(categoryMapper::toDto)
+                .toList();
+        return ResponseEntity.ok(categories);
+    }
+}
