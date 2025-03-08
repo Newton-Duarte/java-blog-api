ba2ff18cdf180f50b9a5e5d450f5a1925cbbe834
Newton Duarte Cavalcanti <newton_@hotmail.com>
2025-03-08 17:16:21 -0300

    feat: implement validate token method on authentication service

--- src/main/java/com/newtonduarte/blog_api/services/impl/AuthenticationServiceImpl.java
+++ src/main/java/com/newtonduarte/blog_api/services/impl/AuthenticationServiceImpl.java
@@ -1,6 +1,7 @@
 package com.newtonduarte.blog_api.services.impl;
 
 import com.newtonduarte.blog_api.services.AuthenticationService;
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
@@ -45,6 +46,23 @@ public class AuthenticationServiceImpl implements AuthenticationService {
                 .signWith(getSigningKey(), SignatureAlgorithm.HS256)
                 .compact();
     }
+
+    @Override
+    public UserDetails validateToken(String token) {
+        String username = extractUsername(token);
+        return userDetailsService.loadUserByUsername(username);
+    }
+
+    private String extractUsername(String token) {
+        Claims claims = Jwts.parserBuilder()
+                .setSigningKey(getSigningKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+
+        return claims.getSubject();
+    }
+
 
     private Key getSigningKey() {
         byte[] keyBytes = secretKey.getBytes();
