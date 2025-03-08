4f127a730eb818a0f060925060d33bf130cebe97
Newton Duarte Cavalcanti <newton_@hotmail.com>
2025-03-08 18:07:50 -0300

    feat: add create tags method

--- src/main/java/com/newtonduarte/blog_api/services/TagService.java
+++ src/main/java/com/newtonduarte/blog_api/services/TagService.java
@@ -3,7 +3,9 @@ package com.newtonduarte.blog_api.services;
 import com.newtonduarte.blog_api.domain.entities.Tag;
 
 import java.util.List;
+import java.util.Set;
 
 public interface TagService {
     List<Tag> getTags();
+    List<Tag> createTags(Set<String> tagNames);
 }
