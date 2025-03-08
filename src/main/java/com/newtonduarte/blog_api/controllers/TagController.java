f8c9b400dd8557b9e216e892a9d5cdc76224d517
Newton Duarte Cavalcanti <newton_@hotmail.com>
2025-03-08 18:08:15 -0300

    feat: add create tags endpoint

--- src/main/java/com/newtonduarte/blog_api/controllers/TagController.java
+++ src/main/java/com/newtonduarte/blog_api/controllers/TagController.java
@@ -1,14 +1,14 @@
 package com.newtonduarte.blog_api.controllers;
 
+import com.newtonduarte.blog_api.domain.dtos.CreateTagsRequest;
 import com.newtonduarte.blog_api.domain.dtos.TagResponse;
 import com.newtonduarte.blog_api.domain.entities.Tag;
 import com.newtonduarte.blog_api.mappers.TagMapper;
 import com.newtonduarte.blog_api.services.TagService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -26,6 +26,16 @@ public class TagController {
         List<TagResponse> tagsResponse = tags.stream().map(tagMapper::toTagResponse).toList();
 
         return ResponseEntity.ok(tagsResponse);
+    }
+
+    @PostMapping
+    public ResponseEntity<List<TagResponse>> createTags(@RequestBody CreateTagsRequest createTagsRequest) {
+        List<Tag> savedTags = tagService.createTags(createTagsRequest.getNames());
+        List<TagResponse> createdTagResponses = savedTags.stream().map(tagMapper::toTagResponse).toList();
+        return new ResponseEntity<>(
+                createdTagResponses,
+                HttpStatus.CREATED
+        );
     }
 
 }
