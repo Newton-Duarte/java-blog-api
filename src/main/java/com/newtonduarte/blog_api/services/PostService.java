a8e07af0fd9615197f231b6be8e501e7ccd4e2bd
Newton Duarte Cavalcanti <newton_@hotmail.com>
2025-03-09 11:45:08 -0300

    feat: add create post method

--- src/main/java/com/newtonduarte/blog_api/services/PostService.java
+++ src/main/java/com/newtonduarte/blog_api/services/PostService.java
@@ -1,5 +1,6 @@
 package com.newtonduarte.blog_api.services;
 
+import com.newtonduarte.blog_api.domain.CreatePostRequest;
 import com.newtonduarte.blog_api.domain.entities.Post;
 import com.newtonduarte.blog_api.domain.entities.User;
 
@@ -9,4 +10,5 @@ import java.util.UUID;
 public interface PostService {
     List<Post> getAllPosts(UUID categoryId, UUID tagId);
     List<Post> getDraftPosts(User user);
+    Post createPost(User user, CreatePostRequest createPostRequest);
 }
